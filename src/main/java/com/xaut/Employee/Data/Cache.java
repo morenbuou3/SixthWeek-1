012b83e9844115db99c5e0705a8acd07116689da
king <369558172@qq.com>
2018-04-24 17:41:20 +0800

    fix: Add postConstruct annotation

--- src/main/java/com/xaut/Employee/Data/Cache.java
+++ src/main/java/com/xaut/Employee/Data/Cache.java
@@ -2,15 +2,19 @@ package com.xaut.Employee.Data;
 
 import com.xaut.Employee.entity.Employee;
 import com.xaut.Employee.entity.Gender;
+import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
+@Configuration
 public class Cache {
 
     private static final List<Employee> cache = new ArrayList<>();
 
-    {
+    @PostConstruct
+    void init() {
         cache.add(new Employee(0, "小明", 20, Gender.MAN));
         cache.add(1, new Employee(1, "小红", 19, Gender.WOMEN));
         cache.add(2, new Employee(2, "小智", 15, Gender.MAN));
