7cf664f7a7fc3c562bf2b5b6451090adeba53f74
king <369558172@qq.com>
2018-04-24 16:19:36 +0800

    feat: Add Cache instead of database

---
+++ src/main/java/com/xaut/Employee/Data/Cache.java
@@ -0,0 +1,24 @@
+package com.xaut.Employee.Data;
+
+import com.xaut.Employee.entity.Employee;
+import com.xaut.Employee.entity.Gender;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cache {
+
+    private static final List<Employee> cache = new ArrayList<>();
+
+    {
+        cache.add(new Employee(0, "小明", 20, Gender.MAN));
+        cache.add(1, new Employee(1, "小红", 19, Gender.WOMEN));
+        cache.add(2, new Employee(2, "小智", 15, Gender.MAN));
+        cache.add(3, new Employee(3, "小刚", 16, Gender.MAN));
+        cache.add(4, new Employee(4, "小霞", 15, Gender.WOMEN));
+    }
+
+    public static List<Employee> getCache() {
+        return cache;
+    }
+}
