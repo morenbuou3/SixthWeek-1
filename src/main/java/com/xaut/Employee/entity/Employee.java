fb2f7c7f971d9104f3a87ebd27e68fa866b52d62
king <369558172@qq.com>
2018-04-24 22:08:24 +0800

    feat: Override Employee Class's equals method

--- src/main/java/com/xaut/Employee/entity/Employee.java
+++ src/main/java/com/xaut/Employee/entity/Employee.java
@@ -1,5 +1,7 @@
 package com.xaut.Employee.entity;
 
+import java.util.Objects;
+
 public class Employee {
 
     private int id;
@@ -49,4 +51,18 @@ public class Employee {
         this.gender = gender;
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (!(obj instanceof Employee)) return false;
+        Employee e = (Employee) obj;
+        return id == e.id && age == e.age
+                && Objects.equals(name, e.name)
+                && Objects.equals(gender, e.gender);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age, gender);
+    }
 }
